2d6c1398b8711db11c86eee57bd1dd29a5b74d16
Junting <342766475@qq.com>
2018-04-19 10:33:50 +0800

    路由API 遵循RESTful API

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const router = new Router();
 
-router.get('/movies/all', async (ctx, next) => {
+router.get('/movies', async (ctx, next) => {
     const Movie = mongoose.model('Movie');
     const movies = await Movie.find({}).sort({
         'meta.createdAt': -1
@@ -15,7 +15,7 @@ router.get('/movies/all', async (ctx, next) => {
     };
 });
 
-router.get('/movies/detail/:id', async (ctx, next) => {
+router.get('/movies/:id', async (ctx, next) => {
     const Movie = mongoose.model('Movie');
     const id = ctx.params.id;
     const movie = await Movie.findOne({_id: id});
